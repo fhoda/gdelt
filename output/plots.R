3fec50d322a24358a1bb047213160e5c6a2500d2
Faisal <hodaf1@tcnj.edu>
2017-03-30 23:50:32 -0400

    totals plot

--- output/plots.R
+++ output/plots.R
@@ -1,18 +1,18 @@
 library(ggplot2)
-data = read.csv('full_data.csv')
+full_data = read.csv('full_data.csv')
 
-###### Total Violence for all years
-ggplot(data, aes(x=Year, y=Total_Violence)) +
-  geom_line() +
-  geom_point() + 
+###### Total Violence & Peaceful Actions for all years
+totals <- ggplot() +
+  geom_line(data=full_data, aes(x=Year, y=Total_Violence), color='red') +
+  geom_point(data=full_data, aes(x=Year, y=Total_Violence), color='red')
+
+totals <- totals + 
+  geom_line(data=full_data, aes(x=Year, y=Total_Peace), color='green') +
+  geom_point(data=full_data, aes(x=Year, y=Total_Peace), color='green') + 
   scale_x_continuous(breaks=seq(1980,2016,2))
 
+print(totals)
 
 
-###### Total Peaceful actions for all years
-# ggplot(data, aes(x=Year, y=Total_Peace)) +
-#   geom_line() +
-#   geom_point() +
-#   scale_x_continuous(breaks=seq(1980,2016,2))
 
-
+##########
